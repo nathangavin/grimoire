c0fd1ed7ab7e1ec25ee3f951066cabc811df90ca
Nathan Gavin <nathangavin987@gmail.com>
2024-11-02 22:15:14 +1300

    added basic logic for getting config file

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,41 @@
+use home::home_dir;
+use std::io::Error;
+
+struct Config {}
+
 fn main() {
     println!("Hello, world!");
+
+    // begin config process
+    let config = match load_config() {
+        Ok(c) => c,
+        Err(a) => {
+            panic!("{:?}", a);
+        }
+    };
+
+    // load data depending on config
+
+    // analyse input args to determine what to do
+}
+
+fn load_config() -> Result<Config, Error> {
+    let mut err_str = String::new();
+    match home_dir() {
+        Some(home) => {
+            let config_path = home.join(".config").join("grimoire").join("config.toml");
+            if config_path.exists() {
+                // yes
+                todo!();
+                return Ok(Config {});
+            } else {
+                err_str = format!("Config file not found at {:?}", config_path);
+            }
+        }
+        None => {
+            err_str = format!("Could not determine home directory");
+        }
+    }
+
+    return Err(Error::other(err_str));
 }
